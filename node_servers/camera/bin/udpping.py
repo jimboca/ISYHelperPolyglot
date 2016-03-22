7ffd333ff8d0a0a89383f5444e618ae0ef4d0a5b
Jim Searle <jimsearle@gmail.com>
2016-03-22 15:23:22 -0700

    Restructure to store all responses and build the clients when done

--- node_servers/camera/bin/udpping.py
+++ node_servers/camera/bin/udpping.py
@@ -20,6 +20,9 @@ SEARCH_REQUEST = pack(">4sH?8sll4s", "MO_I", 0, 0, "", 67108864, 0, "")
 
 def foscam_poll():
 
+    clients = ()
+    clients_by_addr = {}
+
     # Create UDP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 
@@ -32,11 +35,11 @@ def foscam_poll():
     sock.settimeout(PING_INTERVAL)
 
     main_timeout = time.time() + TIMEOUT
-
+    responses = {}
     while time.time() < main_timeout:
 
         # Broadcast our beacon
-        print ("-----------------------\nPinging peers")
+        print ("---------------------------------\nPinging for Foscam's")
         sock.sendto(SEARCH_REQUEST, 0, ("255.255.255.255", PING_PORT_NUMBER))
             
         ping_timeout = time.time() + PING_INTERVAL
@@ -47,37 +50,47 @@ def foscam_poll():
             addr = None
             try:
                 msg, (addr, uport) = sock.recvfrom(PING_MSG_SIZE)
+                # Someone answered our ping, store it.
+                if addr not in responses:
+                    print "- Saving response from %s:%s" % (addr,uport)
+                    responses[addr] = msg
             except socket.timeout:
                 print "- No more reponses"
+    print "All done looking."
 
-            # Someone answered our ping?
-            if addr is not None:
-                print "----\nResponse from: %s:%d" % (addr, uport)
-                print "msg=%s" % msg
-                if len(msg) == 88:
-                    upk = unpack('>23s13s21s4I4b4b4bH?',msg)
-                    #print upk
-                    (header, id, name, ip_i, mask_i, gateway_i, dns_i, r1, r2, r3, r4, sysv1, sysv2, sysv3, sysv4, appv1, appv2, appv3, appv4, port, dhcp) = upk
-                    ip      = socket.inet_ntoa(pack('!I',ip_i))
-                    mask    = socket.inet_ntoa(pack('!I',mask_i))
-                    gateway = socket.inet_ntoa(pack('!I',gateway_i))
-                    dns     = socket.inet_ntoa(pack('!I',dns_i))
-                    id      = id.rstrip('\x00')
-                    name   = name.rstrip('\x00')
-                    print "  Foscam Info: "
-                    print "   ID:%s" % id
-                    print "   Name:%s" % name
-                    print "   IP:%s" % ip
-                    print "   mask:%s" % mask
-                    print "   gateway:%s" % gateway
-                    print "   dns:%s" % dns
-                    print "   reserve:%d.%d.%d.%d" % (r1, r2, r3, r4)
-                    print "   sys:%d.%d.%d.%d" % (sysv1, sysv2, sysv3, sysv4)
-                    print "   app:%d.%d.%d.%d" % (appv1, appv2, appv3, appv4)
-                    print "   port:%s" % port
-                    print "   dhcp:%s" % dhcp
-                else:
-                    print "   Ignored"
+    for addr, msg in responses.iteritems():
+
+        print "----\nResponse from: %s" % (addr)
+        if msg == SEARCH_REQUEST:
+            print "  ignore my echo"
+        elif len(msg) == 88:
+            #print "msg=%s" % msg
+            upk = unpack('>23s13s21s4I4b4b4bH?',msg)
+            #print upk
+            (header, id, name, ip_i, mask_i, gateway_i, dns_i, r1, r2, r3, r4, s1, s2, s3, s4, a1, a2, a3, a4, port, dhcp) = upk
+            client = { 
+                id:        id.rstrip('\x00'),
+                name:      name.rstrip('\x00'),
+                ip:        socket.inet_ntoa(pack('!I',ip_i)),
+                port:      port,
+                mask:      socket.inet_ntoa(pack('!I',mask_i)), 
+                gateway:   socket.inet_ntoa(pack('!I',gateway_i)), 
+                dns:       socket.inet_ntoa(pack('!I',dns_i)), 
+                reserve:   "%d.%d.%d.%d" % (r1, r2, r3, r4),
+                sys:       "%d.%d.%d.%d" % (s1, s2, s3, s4),
+                app:       "%d.%d.%d.%d" % (a1, a2, a3, a4), 
+                dhcp:      dhcp,
+                reserve_a: (r1, r2, r3, r4),
+                sys_a:     (s1, s2, s3, s4),
+                app_a:     (a1, a2, a3, a4),
+            }
+            print "  Foscam Info: "
+            print client
+            clients.push(client)
+        else:
+            print "   Ignoring message of size " + str(len(msg))
+
+    return clients
 
 if __name__ == '__main__':
     foscam_poll()
